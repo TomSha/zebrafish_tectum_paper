b060339239f6c9ef1decc0a19cbebbec0a6987cf
Tom Shallcross (home) <thomas.shallcross@outlook.com>
2023-06-23 11:25:36 +0100

    updated run_analysis

--- 0_run_analysis.R
+++ 0_run_analysis.R
@@ -4,22 +4,28 @@ library(parallel)
 library(gtools)
 
 source("1_preprocess.R")
+source("2_binarise_traces.R")
+
+np <- import("numpy")
 
 # Define constants 
-n_planes <- 7
-ac_freq <- 7.28
-delay <- 0
-n_pixels <- 256
+n_planes <- 7	# Number of imaged planes (one fewer than the total number of imaging planes as the 2photon automatically adds a throw away plane)
+ac_freq <- 7.28	# The acquisition frequency of the 2photon imaging
+delay <- 0 	# The delay between starting imaging and starting the visual stimulation 
+n_pixels <- 256	# The n_pixel x n_pixel resolution of the imaging session
 
-# read in the prefix which defines each experiment
+# Each prefix defines a separate experiment
 prefix_list <- read.table("prefix_list")$V1
 prefix<-"200122_F1_PVN0" 
 
-np <- import("numpy")
+# The main directory defines the path to where the data from all experiments are stored
+# the structure of the data for a given experiment should be: main_directory/prefix/suite2p/combined/
 user <- Sys.info()[names(Sys.info()) == "user"]
 main_directory <- paste("/media/", user, "/Samsung_T5/Stimulus_Barrage/", sep = "")
 
 preprocess_dat(prefix)
+
+binarise_traces(prefix)
 
 
 
